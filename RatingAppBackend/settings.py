a0fb70a5d5b5b36d8e6ba6ac69c58ef9cf0150bc
nahuelba <70066008+nahuelba@users.noreply.github.com>
2021-04-10 14:20:05 -0300

    Update settings.py

--- RatingAppBackend/settings.py
+++ RatingAppBackend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't+dmvrl=4cd*)w_(6v$y*()n0l#4joms(3iz3#ldd2jbb=_vnf'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
