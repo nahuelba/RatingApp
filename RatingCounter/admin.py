77b0c9aa54d4db6cddf26f96d17ba9080c4c15f8
nahuelba <70066008+nahuelba@users.noreply.github.com>
2021-04-13 16:59:00 -0300

    Delete RatingCounter directory

--- RatingCounter/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import RatingCounterModel
-
-# Register your models here.
-class RatingCounterAdmin(admin.ModelAdmin):
-    readonly_fields = (['created', 'modified'])
-
-
-admin.site.register(RatingCounterModel, RatingCounterAdmin)
--- RatingCounter/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RatingcounterConfig(AppConfig):
-    name = 'RatingCounter'
