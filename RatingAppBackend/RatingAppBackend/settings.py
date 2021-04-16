d51b856b25ac4e329fd9133790f0bbd7a128501e
nahuelba <nahuelbarreiro@gmail.com>
2021-04-16 15:26:52 -0300

    aded database

--- RatingAppBackend/RatingAppBackend/settings.py
+++ RatingAppBackend/RatingAppBackend/settings.py
@@ -82,15 +82,15 @@ WSGI_APPLICATION = 'RatingAppBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 DATABASES = {
-    'default' : dj_database_url.config()
+  'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
+
+db_from_venv = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_venv)
 
 
 # Password validation
