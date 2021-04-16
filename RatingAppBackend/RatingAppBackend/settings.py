04f6d8066de39deb6ae1dfccc254dccd6422adac
nahuelba <70066008+nahuelba@users.noreply.github.com>
2021-04-16 16:19:07 -0300

    Update settings.py

--- RatingAppBackend/RatingAppBackend/settings.py
+++ RatingAppBackend/RatingAppBackend/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-import dj_database_url
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -89,8 +86,6 @@ DATABASES = {
    }
 }
 
-db_from_venv = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_venv)
 
 
 # Password validation
